17489f372180f0c743291076596c3caf239eae84
sanghoon302 <57612132+sanghoon302@users.noreply.github.com>
2023-05-23 19:18:40 +0900

    Delete test.py

--- IP_Calculate/test.py
+++
@@ -1,8 +0,0 @@
-test_list = ["kim", "kim", "kim", "kim", "park", "lee", "ryu", "yang", "jeon"]
-result_list = []
-
-for i in test_list:
-    temp = i.replace("kim", "ha")
-    result_list.append(temp)
-    
-print(result_list)
